1aecca42aed513c2e4d2b1ca9c4a9b6db51be7da
SolangeCoughlin <71780399+SolangeCoughlin@users.noreply.github.com>
2021-11-18 12:44:24 -0700

    connecting to db. Removed unnecessary packages

--- app.py
+++ app.py
@@ -1,33 +1,80 @@
 from flask import Flask, render_template, request, redirect
-from db_connector import connect_to_database, execute_query
+from flask_mysqldb import MySQL
+#from db_connector import connect_to_database, execute_query
 import os
 
 # Configuration
  
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_coughlis'
+app.config['MYSQL_PASSWORD'] = '8340'
+app.config['MYSQL_DB'] = 'cs340_coughlis'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
 # Variable to store name of all Tables, to facilitate dropping all when starting up.
 table_names = ["Addresses", "Procedures","Departments","Doctors", "Patients", "Appointments", "Doctors_Procedures"]
 
-# Function to execute initial table creation queries
-def create_tables(sql_file):
+# This function receives a file path as a string, and returns a list containing a command written in the file in each index.
+# The function expect the file to have each query on it's own, single line, ending with a ;. There must not be additional new lines between queries.
+def make_commands(sql_file):
     fd = open(sql_file, 'r')
     query_file = fd.read()
     fd.close()
 
-    creationCommands = query_file.split(';')
-    print(creationCommands)
+    creationCommands = query_file.split(';\n')
+
+    return creationCommands
+
+def insert_sample_data():
+    cur = mysql.connection.cursor()
+
+    #Insert Addresses
+    cur.execute('INSERT INTO Addresses (streetAddress, city, state, zipCode) VALUES ("123 Party Street","Cool Town", "WA", "54321")')
+    cur.execute('INSERT INTO Addresses (streetAddress, city, state, zipCode) VALUES ("999 Random House Lane","Wherever", "GA", "01010")')
+    cur.execute('INSERT INTO Addresses (streetAddress, city, state, zipCode) VALUES ("1 More Court","Beverly Hills", "WI", "44444")')
+
+    # Insert Patients
+    cur.execute('INSERT INTO Patients (patientFirst, patientLast, patientDOB, patientDoc) VALUES ("Zachary","Zucchini", "1994-12-12", 1)')
+    cur.execute('INSERT INTO Patients (patientFirst, patientLast, patientDOB, patientDoc) VALUES ("Andrew","Armadillo", "1983-05-03", 1)')
+    cur.execute('INSERT INTO Patients (patientFirst, patientLast, patientDOB, patientDoc) VALUES ("Sally","Ride", "1951-05-26", 2)')
+
+    # Insert Procedures
+
+    # Insert Departments
+
+    # Insert Doctors
+
+    # Insert Doctors_Procedures
+
+    # Insert Appointments
 
 # Routes 
 @app.route('/')
 def root():
-    db_connection = connect_to_database()
-
+    # Connect to database
+    cur = mysql.connection.cursor()
+    
+    # Get a list of commands to run to create tables
+    creationCommands = make_commands('./sql_queries/table_creation_queries.sql')
+    
+    # Disable foreign key checks temporarily, because foreign key collisions are not important when dropping all entities
+    cur.execute("SET FOREIGN_KEY_CHECKS=0")
+    
     for table in table_names:
         drop_query = "DROP TABLE IF EXISTS %s" % table
-        execute_query(db_connection, drop_query)
-
+        cur.execute(drop_query)
     
+    # Re-enable foreign key checks after dropping all tables
+    cur.execute("SET FOREIGN_KEY_CHECKS=1")
+
+    for command in creationCommands:
+        cur.execute(command)
+    
+    mysql.connection.commit()
 
     return render_template('home.html')
 
@@ -56,10 +103,19 @@ def departments():
 def appointments():
     return render_template('appointments.html')
 
-# @app.route('/addresses', methods=['PUT', 'POST', 'DELETE'])
-@app.route('/addresses')
+@app.route('/addresses', methods=['GET','PUT', 'POST', 'DELETE'])
+#@app.route('/addresses')
 def addresses():
-    return render_template('addresses.html')
+    cur = mysql.connection.cursor()
+
+    if request.method == 'PUT':
+        pass
+    
+    cur.execute('SELECT * FROM Addresses')
+    result = cur.fetchall()
+    mysql.connection.commit()
+    print(result)
+    return render_template('addresses.html', rows=result)
 
 # @app.route('/doctors-procedures', methods=['PUT', 'POST', 'DELETE'])
 @app.route('/doctors-procedures')
@@ -70,5 +126,5 @@ def doctors_procedures():
 
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 55557))
-    
-    app.run(host="flip1.engr.oregonstate.edu", port=51515, debug=False) 
+    app.run(port=12345, debug= True)
+    #app.run(host="flip1.engr.oregonstate.edu", port=51515, debug=False) 
--- sql_queries/table_creation_queries.sql
+++ sql_queries/table_creation_queries.sql
@@ -1,61 +1,16 @@
-CREATE TABLE Addresses(
-	addressID int(11) AUTO_INCREMENT PRIMARY KEY,
-    streetAddress VARCHAR(80) NOT NULL,
-    city VARCHAR(80) NOT NULL,
-    state VARCHAR(80) NOT NULL,
-    zipCode VARCHAR(80) NOT NULL
-);
+CREATE TABLE Addresses(addressID int(11) AUTO_INCREMENT PRIMARY KEY,streetAddress VARCHAR(80) NOT NULL,city VARCHAR(80) NOT NULL,state VARCHAR(80) NOT NULL,zipCode VARCHAR(80) NOT NULL);
 
-CREATE TABLE Procedures(
-	procedureID int(11) AUTO_INCREMENT PRIMARY KEY,
-    procedureName VARCHAR(80) NOT NULL,
-    inPatient BOOLEAN NOT NULL DEFAULT 0
-);
+CREATE TABLE Procedures(procedureID int(11) AUTO_INCREMENT PRIMARY KEY,procedureName VARCHAR(80) NOT NULL,inPatient BOOLEAN NOT NULL DEFAULT 0);
 
-CREATE TABLE Departments(
-	departmentID int(11) AUTO_INCREMENT PRIMARY KEY,
-    departmentName VARCHAR(80) NOT NULL,
-    departmentHead int(11),
-    addressID int(11) NOT NULL,
-    FOREIGN KEY (addressID) REFERENCES Addresses(addressID)
-);
+CREATE TABLE Departments(departmentID int(11) AUTO_INCREMENT PRIMARY KEY,departmentName VARCHAR(80) NOT NULL,departmentHead int(11),addressID int(11) NOT NULL,FOREIGN KEY (addressID) REFERENCES Addresses(addressID));
 
-CREATE TABLE Doctors(
-	doctorID int(11) AUTO_INCREMENT PRIMARY KEY,
-    doctorFirst VARCHAR(80) NOT NULL,
-    doctorLast VARCHAR(80) NOT NULL,
-    doctorDOB DATE NOT NULL,
-    departmentID int(11) NOT NULL,
-    FOREIGN KEY (departmentID) REFERENCES Departments(departmentID)
-);
+CREATE TABLE Doctors(doctorID int(11) AUTO_INCREMENT PRIMARY KEY,doctorFirst VARCHAR(80) NOT NULL,doctorLast VARCHAR(80) NOT NULL,doctorDOB DATE NOT NULL,departmentID int(11) NOT NULL,FOREIGN KEY (departmentID) REFERENCES Departments(departmentID));
 
 -- This must be done as an alteration after Doctors is created, because Doctors must exist for the FOREIGN KEY statement to reference it.
-ALTER TABLE Departments
-ADD FOREIGN KEY (departmentHead) REFERENCES Doctors(doctorID);
+ALTER TABLE Departments ADD FOREIGN KEY (departmentHead) REFERENCES Doctors(doctorID);
 
-CREATE TABLE Patients(
-	patientID int(11) AUTO_INCREMENT PRIMARY KEY,
-    patientFirst VARCHAR(80) NOT NULL,
-    patientLast VARCHAR(80) NOT NULL,
-    patientDOB DATE NOT NULL,
-    patientDoc int(11),
-    FOREIGN KEY (patientDoc) REFERENCES Doctors(doctorID)
-);
+CREATE TABLE Patients(patientID int(11) AUTO_INCREMENT PRIMARY KEY,patientFirst VARCHAR(80) NOT NULL,patientLast VARCHAR(80) NOT NULL,patientDOB DATE NOT NULL,patientDoc int(11),FOREIGN KEY (patientDoc) REFERENCES Doctors(doctorID));
 
-CREATE TABLE Appointments(
-	appointmentID int(11) AUTO_INCREMENT PRIMARY KEY,
-    patientID int(11) NOT NULL,
-    doctorID int(11) NOT NULL,
-    procedureID int(11) NOT NULL,
-    appointmentDate DATE NOT NULL,
-    FOREIGN KEY (patientID) REFERENCES Patients(patientID),
-    FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID),
-    FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID)
-);
+CREATE TABLE Appointments(appointmentID int(11) AUTO_INCREMENT PRIMARY KEY,patientID int(11) NOT NULL,doctorID int(11) NOT NULL,procedureID int(11) NOT NULL,appointmentDate DATE NOT NULL,FOREIGN KEY (patientID) REFERENCES Patients(patientID),FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID),FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID));
 
-CREATE TABLE Doctors_Procedures(
-    procedureID int(11) NOT NULL,
-    doctorID int(11) NOT NULL,
-    FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID),
-    FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID)
-);
+CREATE TABLE Doctors_Procedures(procedureID int(11) NOT NULL,doctorID int(11) NOT NULL,FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID),FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID));
