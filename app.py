90eb06fae7ae2cbe06617eb95309c6084f7ab376
JerameKim <49412416+JerameKim@users.noreply.github.com>
2021-11-18 19:06:47 -0800

    More Heroku

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ import os
 # Configuration
  
 app = Flask(__name__)
-# mysql = MySQL(app)
-mysql = MySQL()
+mysql = MySQL(app)
+# mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_USER'] = 'b5144e26b93e3c'
-app.config['MYSQL_PASSWORD'] = '2e4abfe4'
-app.config['MYSQL_DB'] = 'heroku_5234e1c57267f61'
-app.config['MYSQL_HOST'] = 'us-cdbr-east-04.cleardb.com'
+# app.config['MYSQL_USER'] = 'b5144e26b93e3c'
+# app.config['MYSQL_PASSWORD'] = '2e4abfe4'
+# app.config['MYSQL_DB'] = 'heroku_5234e1c57267f61'
+# app.config['MYSQL_HOST'] = 'us-cdbr-east-04.cleardb.com'
 mysql.init_app(app)
 
-# app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-# app.config['MYSQL_USER'] = 'cs340_kimjera'
-# app.config['MYSQL_PASSWORD'] = '1572'
-# app.config['MYSQL_DB'] = 'cs340_kimjera'
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_kimjera'
+app.config['MYSQL_PASSWORD'] = '1572'
+app.config['MYSQL_DB'] = 'cs340_kimjera'
 
 # app.config['MYSQL_USER'] = 'cs340_coughlis'
 # app.config['MYSQL_PASSWORD'] = '8340'
@@ -369,6 +369,6 @@ def show_doctors_procedures():
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 55557))
+    # port = int(os.environ.get('PORT', 55557))
     # app.run(port=12345, debug= True)
-    app.run(host="flip1.engr.oregonstate.edu", port=51515, debug=False) 
+    # app.run(host="flip1.engr.oregonstate.edu", port=51515, debug=False) 
