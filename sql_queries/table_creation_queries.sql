c24d239ec17d9fba483867160a4a6e27fb2970c7
JerameKim <49412416+JerameKim@users.noreply.github.com>
2021-11-28 15:10:20 -0800

    reorganized table creation

--- sql_queries/table_creation_queries.sql
+++ sql_queries/table_creation_queries.sql
@@ -1,21 +1,75 @@
-CREATE TABLE Addresses(addressID int(11) AUTO_INCREMENT PRIMARY KEY,streetAddress VARCHAR(80) NOT NULL,city VARCHAR(80) NOT NULL,state VARCHAR(80) NOT NULL,zipCode VARCHAR(80) NOT NULL);
+CREATE TABLE `Addresses`(
+    `addressID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `streetAddress` VARCHAR(80) NOT NULL,
+    `city` VARCHAR(80) NOT NULL,
+    `state` VARCHAR(80) NOT NULL,
+    `zipCode` VARCHAR(80) NOT NULL);
 
-CREATE TABLE Procedures(procedureID int(11) AUTO_INCREMENT PRIMARY KEY,procedureName VARCHAR(80) NOT NULL,inPatient BOOLEAN NOT NULL DEFAULT 0);
+CREATE TABLE `Procedures`(
+    `procedureID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `procedureName` VARCHAR(80) NOT NULL,
+    `inPatient` BOOLEAN NOT NULL DEFAULT 0);
 
-CREATE TABLE Departments(departmentID int(11) AUTO_INCREMENT PRIMARY KEY,departmentName VARCHAR(80) NOT NULL,departmentHead int(11),addressID int(11) NOT NULL,FOREIGN KEY (addressID) REFERENCES Addresses(addressID));
+CREATE TABLE `Departments`(
+    `departmentID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `departmentName` VARCHAR(80) NOT NULL,
+    `departmentHead` int(11),
+    `addressID` int(11) NOT NULL,
+    FOREIGN KEY (`addressID`) 
+    REFERENCES `Addresses`(`addressID`));
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE);
 
-CREATE TABLE Doctors(doctorID int(11) AUTO_INCREMENT PRIMARY KEY,doctorFirst VARCHAR(80) NOT NULL,doctorLast VARCHAR(80) NOT NULL,doctorDOB DATE NOT NULL,departmentID int(11) NOT NULL,FOREIGN KEY (departmentID) REFERENCES Departments(departmentID));
+CREATE TABLE `Doctors`(
+    `doctorID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `doctorFirst` VARCHAR(80) NOT NULL,
+    `doctorLast` VARCHAR(80) NOT NULL,
+    `doctorDOB` DATE NOT NULL,
+    `departmentID` int(11) NOT NULL,
+    FOREIGN KEY (`departmentID`) REFERENCES `Departments`(`departmentID`));
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE);
 
 -- This must be done as an alteration after Doctors is created, because Doctors must exist for the FOREIGN KEY statement to reference it.
-ALTER TABLE Departments ADD FOREIGN KEY (departmentHead) REFERENCES Doctors(doctorID);
+ALTER TABLE `Departments` ADD FOREIGN KEY (`departmentHead`) REFERENCES `Doctors`(`doctorID`);
 
-CREATE TABLE Patients(patientID int(11) AUTO_INCREMENT PRIMARY KEY,patientFirst VARCHAR(80) NOT NULL,patientLast VARCHAR(80) NOT NULL,patientDOB DATE NOT NULL,patientDoc int(11),FOREIGN KEY (patientDoc) REFERENCES Doctors(doctorID));
+CREATE TABLE `Patients`(
+    `patientID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `patientFirst` VARCHAR(80) NOT NULL,
+    `patientLast` VARCHAR(80) NOT NULL,
+    `patientDOB` DATE NOT NULL,
+    `patientDoc` int(11),
+    FOREIGN KEY (`patientDoc`) REFERENCES `Doctors`(`doctorID`) );
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE);
 
-CREATE TABLE Appointments(appointmentID int(11) AUTO_INCREMENT PRIMARY KEY,patientID int(11) NOT NULL,doctorID int(11) NOT NULL,procedureID int(11) NOT NULL,appointmentDate DATE NOT NULL,FOREIGN KEY (patientID) REFERENCES Patients(patientID),FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID),FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID));
+CREATE TABLE `Appointments`(
+    `appointmentID` int(11) AUTO_INCREMENT PRIMARY KEY,
+    `patientID` int(11) NOT NULL,
+    `doctorID` int(11) NOT NULL,
+    `procedureID` int(11) NOT NULL,
+    `appointmentDate` DATE NOT NULL,
+    FOREIGN KEY (`patientID`) REFERENCES `Patients`(`patientID`),
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE,
+    FOREIGN KEY (`doctorID`) REFERENCES `Doctors`(`doctorID`), 
+        -- ON DELETE SET NULL 
+        -- ON UPDATE CASCADE,
+    FOREIGN KEY (`procedureID`) REFERENCES `Procedures`(`procedureID`));
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE);
 
-CREATE TABLE Doctors_Procedures(procedureID int(11) NOT NULL,doctorID int(11) NOT NULL,FOREIGN KEY (procedureID) REFERENCES Procedures(procedureID),FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID));
+CREATE TABLE `Doctors_Procedures`(
+    `procedureID` int(11) NOT NULL,
+    `doctorID` int(11) NOT NULL,
+    FOREIGN KEY (`procedureID`) REFERENCES `Procedures`(`procedureID`),
+        -- ON DELETE SET NULL 
+        -- ON UPDATE CASCADE,
+    FOREIGN KEY (`doctorID`) REFERENCES `Doctors`(`doctorID`));
+        -- ON DELETE SET NULL
+        -- ON UPDATE CASCADE);
 
--- Trying to add sample data here
+-- ######## Sample data ######## -- 
     -- # Insert Addresses
 INSERT INTO Addresses (streetAddress, city, state, zipCode) VALUES ("123 Party Street","Cool Town", "WA", "54321");
 INSERT INTO Addresses (streetAddress, city, state, zipCode) VALUES ("999 Random House Lane","Wherever", "GA", "01010");
